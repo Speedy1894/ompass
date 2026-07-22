01e28620ee38525944836d55a0eea35c8e6c8ea6
Speedy1894 <speedy1894@duck.com>
2026-07-21 22:37:42 -0500

    Create basic stderr-logging function and `input()` wrapper

--- ompass.py
+++ ompass.py
@@ -5,3 +5,34 @@ import sys
 import termios
 import time
 
+
+def print_log(mode:str, *args, sep:str=" ", end:str"\n"):
+    r"""Print a message to stderr with a preceding type specifier.
+    
+    All arguments other than `mode` mirror those of the `print()`
+    builtin.
+    
+    mode:str: [DEIW] First character of message type
+    """
+    match mode:
+        case "D": typestr:str = "36mDEBUG"
+        case "E": typestr:str = "31mERROR"
+        case "I": typestr:str = "32mINFO"
+        case "W": typestr:str = "33mWARN"
+        case _: typestr:str = "30m    "
+    
+    print(f"[\x1b[{typestr}\x1b[39m]", *args,
+          sep=sep, end=end, file=sys.stderr)
+
+
+def safe_input(prompt:str=""):
+    r"""Read from stdin with safety for KeyboardInterrupt and EoF.
+    
+    If either of the above exceptions are raised, they will be handled
+    by running `sys.exit()` instead of printing a traceback.
+    """
+    try:
+        return input(prompt)
+    except (KeyboardInterrupt, EOFError):
+        print(file=sys.stderr)  # Needed to proceed to next line
+        sys.exit(130)
